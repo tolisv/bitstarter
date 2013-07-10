0d347ca3dcda324a9f037ecbdfa8b5f34e1f71d3
Tolis Vossos <tolisv@gmail.com>
2013-07-10 02:27:56 +0000

    Edit web.js to read from index.html and add index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var msg = fs.readFileSync('index.html').toString();
+
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
